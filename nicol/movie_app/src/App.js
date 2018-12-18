e8476106a5c04d1d8c2f8b52a07d6d4945d6a11b
yoonucho <yoonu78@naver.com>
2018-12-19 02:36:16 +0900

    추가:setState,...this.state.movies (very very awesomegit status)

--- nicol/movie_app/src/App.js
+++ nicol/movie_app/src/App.js
@@ -2,40 +2,48 @@ import React, { Component } from "react";
 import "./App.css";
 import Movie from "./Movie";
 
-const movies = [
-  {
-    title: "Matrix",
-    poster:
-      "https://images-na.ssl-images-amazon.com/images/I/813dE2pH7XL._SY355_.jpg"
-  },
-  {
-    title: "Full Metal Jacket",
-    poster:
-      "https://images-na.ssl-images-amazon.com/images/I/81U3cu+0RAL._RI_.jpg"
-  },
-  {
-    title: "Oldboy",
-    poster:
-      "https://resizing.flixster.com/dQKMU1lG417HWPtxFYuPvHkfhYE=/206x305/v1.bTsxMTE3NjY0NjtqOzE3OTk0OzEyMDA7MTQwMDsyMTAw"
-  },
-  {
-    title: "Star Wars",
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BMTUzNDY0NjY4Nl5BMl5BanBnXkFtZTgwNjY4MTQ0NzE@._V1_CR0,60,640,360_AL_UX477_CR0,0,477,268_AL_.jpg"
-  }
-];
 class App extends Component {
   // Render : componentWillMount() -> render() -> componentDidMount()
   // Update componentWillReceiveProps() -> shouldComponentUpdate() == true -> componentWillUpdate()-> render() -> componentDidMount()
 
   state = {
-    greeting: "Hello!"
+    greeting: "Hello!",
+    movies: [
+      {
+        title: "Matrix",
+        poster:
+          "https://images-na.ssl-images-amazon.com/images/I/813dE2pH7XL._SY355_.jpg"
+      },
+      {
+        title: "Full Metal Jacket",
+        poster:
+          "https://images-na.ssl-images-amazon.com/images/I/81U3cu+0RAL._RI_.jpg"
+      },
+      {
+        title: "Oldboy",
+        poster:
+          "https://resizing.flixster.com/dQKMU1lG417HWPtxFYuPvHkfhYE=/206x305/v1.bTsxMTE3NjY0NjtqOzE3OTk0OzEyMDA7MTQwMDsyMTAw"
+      },
+      {
+        title: "Star Wars",
+        poster:
+          "https://m.media-amazon.com/images/M/MV5BMTUzNDY0NjY4Nl5BMl5BanBnXkFtZTgwNjY4MTQ0NzE@._V1_CR0,60,640,360_AL_UX477_CR0,0,477,268_AL_.jpg"
+      }
+    ]
   };
-
+  // 리스트를 하나 추가하고 싶다면
   componentDidMount() {
     setTimeout(() => {
       this.setState({
-        greeting: "Hello again!"
+        movies: [
+          // 전체 영화 리스트
+          ...this.state.movies,
+          {
+            title: "Transpotting",
+            poster:
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHjZTfWogMeMsXcxRd3eOcYs1ijuJDeKUSWRDoIj6v-96VQHcq1A"
+          }
+        ]
       });
     }, 5000);
   }
@@ -43,8 +51,7 @@ class App extends Component {
     console.log("did render");
     return (
       <div className="App">
-        {this.state.greeting}
-        {movies.map((movie, index) => {
+        {this.state.movies.map((movie, index) => {
           return (
             <Movie title={movie.title} poster={movie.poster} key={index} />
           );
