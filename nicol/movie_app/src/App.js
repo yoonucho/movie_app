4c96da01f5f2e25979ffccc77b6a6aa8f33d4075
yoonucho <yoonu78@naver.com>
2018-12-19 16:56:33 +0900

    추가: Promise(.then, .catch)

--- nicol/movie_app/src/App.js
+++ nicol/movie_app/src/App.js
@@ -10,7 +10,13 @@ class App extends Component {
 
   // 리스트를 하나 추가하고 싶다면
   componentDidMount() {
-    fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating");
+    // fetch가 좋은 이유 url을 ajax로 심플하게 불러올 수 있어서
+    fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")
+      //   .then(response => console.log(response))
+      .then(response => response.json())
+      .then(json => console.log(json))
+      //catch를 써서 에러가 나면 잡아서 설명해라.
+      .catch(err => console.log(err));
   }
 
   _renderMovies = () => {
