afc3e4692e10f6fb3ef7559357063330f8592418
yoonucho <yoonu78@naver.com>
2018-12-19 17:31:22 +0900

    추가: Async Await

--- nicol/movie_app/src/App.js
+++ nicol/movie_app/src/App.js
@@ -10,21 +10,46 @@ class App extends Component {
 
   // 리스트를 하나 추가하고 싶다면
   componentDidMount() {
-    // fetch가 좋은 이유 url을 ajax로 심플하게 불러올 수 있어서
-    fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")
-      //   .then(response => console.log(response))
-      .then(response => response.json())
-      .then(json => console.log(json))
-      //catch를 써서 에러가 나면 잡아서 설명해라.
-      .catch(err => console.log(err));
+    this._getMovies();
+    //   .then(response => console.log(response))
+    //   .then(json => {
+    // 	  this.setState({
+    // 		  movies: json.data.movies
+    // 	  })
+    // 	  .then(()=> .then())
+    // 	  // CALLBACK HELL!
+    //   })
+    //catch를 써서 에러가 나면 잡아서 설명해라.
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />;
+    const movies = this.state.movies.map(movie => {
+      console.log(movie);
+      return (
+        <Movie
+          title={movie.title}
+          poster={movie.medium_cover_image}
+          key={movie.id}
+        />
+      );
     });
     return movies;
   };
+
+  _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    });
+  };
+
+  _callApi = () => {
+    return fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")
+      .then(response => response.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err));
+  };
+
   render() {
     return (
       <div className="App">
